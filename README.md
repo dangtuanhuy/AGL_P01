2255e02e640376b9aaabda5a9229751e5836b3d6
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-20 16:31:09 +0700

    save

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyProject01
+# MyDreamP01
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.3.9.
 
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to my-project01!');
+    expect(page.getTitleText()).toEqual('Welcome to my-dream-p01!');
   });
 
   afterEach(async () => {
