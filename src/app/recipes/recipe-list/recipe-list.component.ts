0e1b1f1d77fb923de5a43ef5b35515ae37941c8f
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-21 11:40:51 +0700

    component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+// Đưa Recipe từ model vào mảng
 export class RecipeListComponent implements OnInit {
-
-  repices = [];
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Recipe', 'Heat oil in a non-stick pan', 'https://i.imgur.com/0Y2w7my.png'),
+    new Recipe('Recipe', 'Heat oil in a non-stick pan', 'https://i.imgur.com/0Y2w7my.png')
+  ];
   constructor() { }
 
   ngOnInit() {
