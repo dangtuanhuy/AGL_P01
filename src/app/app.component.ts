79d1e9ee5f56b3781bac4bd92ceb1463a8aec5b0
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-23 13:03:04 +0700

    out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-dream-p01';
+  loadedFeature = 'recipe';
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
